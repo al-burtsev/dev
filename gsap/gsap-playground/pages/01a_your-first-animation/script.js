353a54b3592a0ce984b7434c2f0c52873f6a3e0c
al-burtsev <burtsev.aleksandr9@gmail.com>
2025-09-19 12:54:28 +0300

    pulsing glow animation

--- gsap/gsap-playground/pages/01a_your-first-animation/script.js
+++ gsap/gsap-playground/pages/01a_your-first-animation/script.js
@@ -1,10 +1,3 @@
-// gsap.to('.card', {
-//     rotation: 360,
-//     repeat: -1,
-//     duration: 1,
-//     ease: "linear"
-// })
-
 gsap.to(".card", {
     opacity: 1,
     scale: 1,
@@ -15,14 +8,6 @@ gsap.to(".card", {
             repeat: -1,
             yoyo: true,
             duration: 0.5,
-        });
-
-        // Pulsing glow animation
-        gsap.to(".card", {
-            boxShadow: "0 20px 80px rgba(29, 209, 161, 0.6)",
-            repeat: -1,
-            yoyo: true,
-            duration: 1,
         });
     },
 });
