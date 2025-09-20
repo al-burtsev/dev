f1a4cfed0989129062bae55fbe8b68852a5bd52e
al-burtsev <burtsev.aleksandr9@gmail.com>
2025-09-20 15:43:56 +0300

    expo-easing

--- gsap/gsap-playground/pages/08a_expo-easing/script.js
+++ gsap/gsap-playground/pages/08a_expo-easing/script.js
@@ -1,3 +1,5 @@
+import gsap from "gsap";
+
 // Select the main floating action button (FAB)
 const fab = document.querySelector(".fab");
 
@@ -30,10 +32,28 @@ fab.addEventListener("click", () => {
       const y = -Math.sin(angle) * radius;
 
       // 🔜 Animation will go here
+      gsap.to(child, {
+        x,
+        y,
+        opacity: 1,
+        rotation: 360,
+        duration: 0.5,
+        ease: 'expo.out'
+      })
     });
   } else {
     children.forEach((child) => {
       // 🔜 Collapse animation will go here
+      gsap.to(child, {
+        x: 0,
+        y: 0,
+        opacity: 0,
+        rotation: 0,
+        scale: 0.7,
+        pointerEvents: 'none',
+        duration: 0.3,
+        ease: 'expo.in'
+      })
     });
   }
 });
