22056d0a4d5e38498c610580131ba1af4d320548
Alexandr Burtsev <burtsev.aleksandr9@gmail.com>
2025-09-23 21:03:29 +0300

    done first-video section

--- gsap/gta-landing/src/App.jsx
+++ gsap/gta-landing/src/App.jsx
@@ -2,6 +2,7 @@ import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/all'
 import Header from './sections/Header'
 import Hero from './sections/Hero'
+import FirstVideo from './sections/FirstVideo'
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -11,6 +12,7 @@ const App = () => {
         <main>
             <Header />
             <Hero />
+            <FirstVideo />
         </main>
     )
 }
